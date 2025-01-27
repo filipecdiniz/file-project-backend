f6ad57ced1adc25e7bf57f0be22f84ceb306e5e8
Filipe Diniz <filipecadiniz@gmail.com>
2025-01-27 17:56:10 -0300

    authModule: JwtModule created!

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     providers: [AuthService],
     controllers: [AuthController],
+    imports: [
+        JwtModule.register({
+            global: true,
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '1d' }
+        })
+    ]
 })
 export class AuthModule { }
